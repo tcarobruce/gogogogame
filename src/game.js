1aad54484154175d631dc42a03409f08f5d544e1
tcarobruce <tcarobruce@gmail.com>
2014-01-31 00:04:26 -0800

    add some constants, suppress bg for now

--- src/game.js
+++ src/game.js
@@ -7,12 +7,20 @@ Game = {
             width: 70
         }
     },
+    world: {
+        block_frequency: 0.6,
+        gravity: 0.4
+    },
+    player: {
+        speed: 5,
+        jump: 7
+    },
     spritesheets: [],
     reels: {},
 
     start: function() {
         Crafty.init();
-        Crafty.background("url('http://tcbxyz.com/game/assets/images/ufo_background.jpg')");
+        //Crafty.background("url('http://tcbxyz.com/game/assets/images/ufo_background.jpg')");
         Crafty.scene("loading");
     }
 };
